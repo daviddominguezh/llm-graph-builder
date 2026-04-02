e21d2e32c8a5cd419f6fa7d1b7a571ad902e78f8
David Dominguez <l.david.dominguez.12@gmail.com>
2026-04-02 10:29:51 -0500

    feat: add barrel export for node creation dialogs

---
+++ packages/web/app/components/panels/nodeCreationDialogs/index.ts
@@ -0,0 +1,4 @@
+export { UserNodeDialog } from './UserNodeDialog';
+export { ToolNodeDialog } from './ToolNodeDialog';
+export { IfElseDialog } from './IfElseDialog';
+export { LoopDialog } from './LoopDialog';
