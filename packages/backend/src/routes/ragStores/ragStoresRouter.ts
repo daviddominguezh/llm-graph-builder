b0ddee6901964d6dc8be2b72576b59a5b211e496
David Dominguez <l.david.dominguez.12@gmail.com>
2026-05-11 12:56:19 -0500

    feat(backend): mount rag_files sub-router + search route

--- packages/backend/src/routes/ragStores/ragStoresRouter.ts
+++ packages/backend/src/routes/ragStores/ragStoresRouter.ts
@@ -4,6 +4,8 @@ import { requireAuth } from '../../middleware/auth.js';
 import { handleCreateRagStore } from './createRagStore.js';
 import { handleDeleteRagStore } from './deleteRagStore.js';
 import { handleGetRagStores } from './getRagStores.js';
+import { ragFilesRouter } from './ragFiles/ragFilesRouter.js';
+import { handleSearchChunks } from './ragFiles/searchChunks.js';
 
 export const ragStoresRouter = express.Router();
 ragStoresRouter.use(requireAuth);
@@ -11,3 +13,5 @@ ragStoresRouter.use(requireAuth);
 ragStoresRouter.get('/:orgId', handleGetRagStores);
 ragStoresRouter.post('/', handleCreateRagStore);
 ragStoresRouter.delete('/:storeId', handleDeleteRagStore);
+ragStoresRouter.use('/:storeId/files', ragFilesRouter);
+ragStoresRouter.post('/:storeId/search', handleSearchChunks);
