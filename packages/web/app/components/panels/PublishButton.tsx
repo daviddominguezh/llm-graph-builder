00894fbe269b2d4a299d09550d78a57a48d7a64c
David Dominguez <l.david.dominguez.12@gmail.com>
2026-06-15 11:35:36 -0500

    check ui

--- packages/web/app/components/panels/PublishButton.tsx
+++ packages/web/app/components/panels/PublishButton.tsx
@@ -268,7 +268,7 @@ export function PublishButton(props: PublishButtonProps) {
         {t('publish')}
         <ChevronDown className="size-4" />
       </PopoverTrigger>
-      <PopoverContent side="bottom" align="center" sideOffset={8} className="w-96 mr-1.5 -mt-1 ring-[0.5px]">
+      <PopoverContent side="bottom" align="center" sideOffset={8} className="w-96 mr-1.5 -mt-1">
         <PopoverBody
           agentSlug={agentSlug}
           orgSlug={orgSlug}
